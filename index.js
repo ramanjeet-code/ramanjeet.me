454166e2f7b88fb641592c3d4cd6834791dd6515
Ramanjeet Singh <39863817+gzbsingh@users.noreply.github.com>
2023-09-12 15:35:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Full Stack Developer", "Daydreamer"],
+   strings: ["Java Backend Developer","Problem Solver"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Java Backend Developer", "Daydreamer","programmer"],
+    strings: ["Java Backend Developer","programmer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
